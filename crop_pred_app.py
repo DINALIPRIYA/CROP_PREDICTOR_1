53f2f9c5e4513df072ab24d2bbc1c358fb8b91e0
DINALIPRIYA <dinalipriyanjana@gmail.com>
2025-04-16 17:27:37 +0530

    Update crop_pred_app.py

--- crop_pred_app.py
+++ crop_pred_app.py
@@ -2,16 +2,16 @@ import streamlit as st
 import pandas as pd
 import joblib
 import pickle
-# Load the trained Linear Regression model
+ Load the trained Linear Regression model
 model = joblib.load("crop_prediction_model.pkl")
 
-# Streamlit app
+ Streamlit app
 st.title("Crop Predictor")
 
-# Add an image to the page
-#st.image("C:/Users/dinalik/Desktop/python course/istockphoto-1127372646-612x612.jpg")
+ Add an image to the page
+st.image("C:/Users/dinalik/Desktop/python course/istockphoto-1127372646-612x612.jpg")
 
-# Sidebar with input fields for crop properties
+ Sidebar with input fields for crop properties
 st.sidebar.header("Enter crop Properties")
 Nitrogen = st.sidebar.slider("Nitrogen", 0.0, 160.0, 8.0)
 Pottasium = st.sidebar.slider("Pottasium", 5.0, 205.0, 0.5)
@@ -26,39 +26,39 @@ c=df.label.astype('category')
 targets = dict(enumerate(c.cat.categories))
 crop_labels=list(targets.values())
 crop_labels = [label.capitalize() for label in crop_labels]
-# Predict Crop
+ Predict Crop
 crop_properties = [[Nitrogen, Pottasium, Phosphorus, Temperature,
                     Humidity, pH, Rainfall]]
 predicted_label = model.predict(crop_properties)[0]
-predicted_crop_name = crop_labels[predicted_label]  # Assuming the label is an index
+predicted_crop_name = crop_labels[predicted_label]   Assuming the label is an index
 
 image_paths = {
- 0: 'https://plus.unsplash.com/premium_photo-1661322640130-f6a1e2c36653?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGV8ZW58MHx8MHx8fDA%3D',
- #1: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/banana.jfif',
- #2: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/blackgram.jfif',
- #3: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/chickpea.jpg',
- #4: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/coconut.jpg',
- #5: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/coffee.jpg',
- #6: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/cotton.jfif',
- #7: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/grapes.jpg',
- #8: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/jute.jpg',
- #9: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/KIDNEYBEANS.jpg',
- #10: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/lentils.jpg',
- #11: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/maize.jfif',
- #12: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/mango.jfif',
- #13: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/mothbeans.jfif',
- #14: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/mung.jpg',
- #15: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/muskmelon.jfif',
- #16: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/orange_1.jPG',
- #17: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/papaya.jpg',
- #18: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/pigeon.jpg',
- #19: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/pomegranate.jfif',
- #20: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/rice.jfif',
- #21: 'C:/Users/dinalik/Desktop/python course/CROP_PREDICTOR/crops/watermelon.jpg'
+ 0: 'apple.jfif',
+ 1: 'banana.jfif',
+ 2: 'blackgram.jfif',
+ 3: 'chickpea.jpg',
+ 4: 'coconut.jpg',
+ 5: 'coffee.jpg',
+ 6: 'cotton.jfif',
+ 7: 'grapes.jpg',
+ 8: 'jute.jpg',
+ 9: 'KIDNEYBEANS.jpg',
+ 10: 'lentils.jpg',
+ 11: 'maize.jfif',
+ 12: 'mango.jfif',
+ 13: 'mothbeans.jfif',
+ 14: 'mung.jpg',
+ 15: 'muskmelon.jfif',
+ 16: 'orange_1.jPG',
+ 17: 'papaya.jpg',
+ 18: 'pigeon.jpg',
+ 19: 'pomegranate.jfif',
+ 20: 'rice.jfif',
+ 21: 'watermelon.jpg'
  }
 
 image_path = image_paths.get(predicted_label, None)
-# Display predicted category
+ Display predicted category
 st.image(image_path, caption=f"Crop: {predicted_label}")
 st.title (f"Predicted crop label: ")
 st.title(predicted_crop_name)
